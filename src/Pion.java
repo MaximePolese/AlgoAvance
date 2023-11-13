b8c14e63fa053fc22f4d0485e59de17003904bc9
adil.gahramanli <adil.gahramanli@le-campus-numerique.fr>
2023-11-13 14:08:16 +0100

    Pion et Game

--- src/Pion.java
+++ src/Pion.java
@@ -3,8 +3,8 @@ public class Pion extends Case {
     public Pion(Joueur joueur, int posX, int posY) {
         super(joueur, posX, posY);
     }
-    public void displayPion(Joueur joueur){
-        if (joueur == Game.p1) {
+    public void displayPion(Joueur joueur, TicTacToe TicTacToe){
+        if (joueur == TicTacToe.getP1()) {
             System.out.println("| X |");
         }
         else {
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 
 public class TicTacToe implements Game {
+    private Plateau board;
+
+    private Joueur p1;
+
+    private Joueur p2;
+
 
 
     public TicTacToe () {
-
+        board = new Plateau(3,3);
+        p1 = new Joueur("Edouard");
+        p2 = new Joueur("Maximilien");
     }
 
     @Override
@@ -17,6 +25,10 @@ public class TicTacToe implements Game {
         /* si on a 3 formes similaires, ou 3 positions d'un même joueur qui sont sur une diagonales ou sur un
         côté ou sur une ligne, alors la partie est Over*/
         return false;
+    }
+
+    public Joueur getP1() {
+        return p1;
     }
 
 
