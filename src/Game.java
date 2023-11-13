f258c1996c8f118bbe7ec1667e8bca0e40b5a65d
adil.gahramanli <adil.gahramanli@le-campus-numerique.fr>
2023-11-13 13:50:21 +0100

    Game interface

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,15 @@
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Game {
+
+    boolean nextTurn();
+
+    boolean isOver();
+
+//    ArrayList<Integer> getPossibleMoves();
+
+
+
+
 }
